3cda92fa3487cb2345634d804109c2479b716186
Your Name <you@example.com>
2018-07-23 14:03:34 +0800

    test bsp +periodic

--- MF-Stellar/ps.cpp
+++ MF-Stellar/ps.cpp
@@ -309,6 +309,7 @@ void PeriodicStatistics()
 
     while (1 == 1)
     {
+        printf("Start to sleep...\n");
         std::this_thread::sleep_for(std::chrono::milliseconds(50000));
         time_units++;
 
