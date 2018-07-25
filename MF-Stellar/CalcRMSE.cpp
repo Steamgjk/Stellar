ebbeb46f600002a5f0d459997be511b69c2c6c6f
Your Name <you@example.com>
2018-07-25 12:39:26 +0800

    ok

--- MF-Stellar/CalcRMSE.cpp
+++ MF-Stellar/CalcRMSE.cpp
@@ -1,41 +1,11 @@
-#include <iostream>
+#include "stellar_common.h"
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <stdio.h>
-#include <string.h>
-#include <cmath>
-#include <time.h>
-#include <vector>
-#include <list>
-#include <thread>
-#include <chrono>
-#include <algorithm>
-#include <mutex>
-#include <iostream>
-#include <fstream>
-#include <sys/time.h>
-#include <map>
-
-
-#define FILE_NAME "./yahoo-output/train-"
-#define TEST_NAME "/home/shuai/oneword/validationDS"
-
-#define N 1000990
-#define M 624961
-#define K  100 //主题个数
-
-
-int ITER_NUM  = 2000;
-int PORTION_NUM = 4;
-double P[N][K];
-double Q[K][M];
 int WORKER_NUM = 16;
+int row_lens[20];
+int col_lens[20];
+int row_unit = ROW_UNIT * (ROW_PS / WORKER_NUM);
+int col_unit = COL_UNIT * (COL_RS / WORKER_NUM);
+
 using namespace std;
 
 
@@ -44,6 +14,57 @@ int check_points[40] = {2, 7, 13, 18, 24, 30, 36, 41, 47, 53, 60, 65, 70, 75, 82
 std::vector<int> vec_uids;
 std::vector<int> vec_mids;
 std::vector<float> vec_rates;
+
+Block Pblocks[30];
+Block Qblocks[30];
+
+
+void partitionP(Block * Pblocks)
+{
+    int i = 0;
+    int height = row_unit;
+    int last_height = N - (WORKER_NUM - 1) * height;
+    printf("P height=%d  last_height=%d\n", height, last_height );
+    for (i = 0; i < WORKER_NUM; i++)
+    {
+        Pblocks[i].block_id = i;
+        Pblocks[i].data_age = 0;
+        Pblocks[i].eles.clear();
+        Pblocks[i].height = height;
+        if ( i == WORKER_NUM - 1)
+        {
+            Pblocks[i].height = last_height;
+        }
+        Pblocks[i].sta_idx = row_lens[i];
+        Pblocks[i].ele_num = Pblocks[i].height * K;
+        Pblocks[i].eles.resize(Pblocks[i].ele_num);
+    }
+
+}
+
+void partitionQ(Block * Qblocks)
+{
+    int i = 0;
+    int height = col_unit;
+    int last_height = M - (WORKER_NUM - 1) * height;
+    printf("Q height=%d  last_height=%d\n", height, last_height );
+    for (i = 0; i < WORKER_NUM; i++)
+    {
+        Qblocks[i].block_id = i + WORKER_NUM;
+        Qblocks[i].data_age = 0;
+        Qblocks[i].eles.clear();
+        Qblocks[i].height = height;
+        if ( i == WORKER_NUM - 1)
+        {
+            Qblocks[i].height = last_height;
+        }
+        Qblocks[i].sta_idx = col_lens[i];
+        Qblocks[i].ele_num = Qblocks[i].height * K;
+        Qblocks[i].eles.resize(Qblocks[i].ele_num);
+
+    }
+
+}
 
 void LoadTestRating()
 {
@@ -84,6 +105,8 @@ int main(int argc, const char * argv[])
     char filename[100];
     //int i = 0;
     //int i = atoi(argv[3]);
+    partitionP(Pblocks);
+    partitionQ(Qblocks);
     for (int log_idx = 0 ; log_idx < 31; log_idx++)
     {
         int i = check_points[log_idx];
@@ -104,16 +127,20 @@ int main(int argc, const char * argv[])
                 printf("open %s\n", filename );
             }
             //getchar();
+            int num = 0;
             while (!ifs.eof())
             {
+                /*
                 for (int kk = 0; kk < K; kk++)
                 {
                     ifs >> P[row_idx][kk];
                 }
                 row_idx++;
-                //if (row_idx % 1000 == 0)
-                // printf("row_idx=%d\n", row_idx);
+                **/
+                ifs >> Pblocks[j].eles[num];
+                num++;
             }
+
             ifs.close();
             //printf("%s read\n", filename );
             //getchar();
@@ -126,15 +153,18 @@ int main(int argc, const char * argv[])
                 getchar();
             }
             double temp;
+            num = 0;
             while (!ifs.eof())
             {
-
-                for (int kk = 0; kk < K; kk++)
-                {
-                    ifs >> Q[kk][col_idx];
-                }
-
-                col_idx++;
+                /*
+                                for (int kk = 0; kk < K; kk++)
+                                {
+                                    ifs >> Q[kk][col_idx];
+                                }
+                                col_idx++;
+                                **/
+                ifs >> Qblocks[j].eles[num];
+                num++;
             }
             ifs.close();
             //printf("%s read\n", filename );
@@ -151,23 +181,34 @@ int main(int argc, const char * argv[])
         while (!ifs.eof())
         {
             ifs >> user_id >> movie_id >> rate;
-
-
             test_cnt++;
-            if (test_cnt % 10000 == 0)
-            {
-                printf("test_cnt=%d\n",  test_cnt);
-            }
-
+            int pblock_idx = user_id / row_unit;
+            int qblock_idx = movie_id / col_unit;
+            int p_ini_idx = user_id - (pblock_idx * row_unit);
+            int q_ini_idx = movie_id - (qblock_idx * col_unit);
             float sum = 0;
-            for (int ii = 0; ii < K; ii++)
+            for (int i = 0; i < K; i++)
             {
-                sum += (P[user_id][ii] * 10) * (Q[ii][movie_id] * 10);
+                sum += (Pblocks[pblock_idx].eles[p_ini_idx * K + i] * 10) * (Qblocks[qblock_idx].eles[q_ini_idx * K + i] * 10);
             }
-            //printf("user_id=%d movie_id=%d rate=%f sum=%f\n", user_id, movie_id, rate, sum  );
-            //getchar();
             rmse += (rate - sum) * (rate - sum);
+            /*
 
+
+                        if (test_cnt % 10000 == 0)
+                        {
+                            printf("test_cnt=%d\n",  test_cnt);
+                        }
+
+                        float sum = 0;
+                        for (int ii = 0; ii < K; ii++)
+                        {
+                            sum += (P[user_id][ii] * 10) * (Q[ii][movie_id] * 10);
+                        }
+                        //printf("user_id=%d movie_id=%d rate=%f sum=%f\n", user_id, movie_id, rate, sum  );
+                        //getchar();
+                        rmse += (rate - sum) * (rate - sum);
+            **/
 
         }
 
