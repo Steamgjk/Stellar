e053d19f06ab70b84d1daf2c4fb0546c705f24bb
Your Name <you@example.com>
2018-07-31 19:28:33 +0800

    time

--- PR-Stellar/stellar_common.h
+++ PR-Stellar/stellar_common.h
@@ -63,8 +63,8 @@ using namespace std;
 
 //#define STELLAR // ini_rmse 46.2740
 //#define BSP_MODE //ini_rmse 46.41
-//#define ASP_MODE //ini_rmse 46.4143   120-110  200-230
-#define SSP_MODE
+#define ASP_MODE //ini_rmse 46.4143   120-110  200-230
+//#define SSP_MODE
 //#define SSP_BOUND (0) //ini_rmse  46.414
 //#define SSP_BOUND (10000) //ini_rmse  46.414
 //#define SSP_BOUND (1) //ini_rmse  46.383
